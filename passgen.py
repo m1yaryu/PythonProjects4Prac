ceb3c9134b7a52d6adb1fce425a865f5be8be942
m1yaryu <127284546+m1yaryu@users.noreply.github.com>
2023-11-18 02:11:33 +0900

    Add files via upload

--- passgen.py
+++ passgen.py
@@ -2,3 +2,11 @@
 import random
 
 Password = ""
+PassChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-+=|\;:<,>.?/'
+
+Len = random.randint(1, 15)
+
+for i in range(Len):
+    Password += random.choice(PassChars)
+
+print(Password)
