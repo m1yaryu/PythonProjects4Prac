851762f4b0bb4a408cdc0174877788283ed22722
m1yaryu <127284546+m1yaryu@users.noreply.github.com>
2023-11-23 00:05:09 +0900

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,14 @@
 import random
 
+#list of cards
 Cards = {"A♠, 2♠, 3♠, 4♠, 5♠, 6♠, 7♠, 8♠, 9♠, 10♠, J♠, Q♠, K♠, A♥, 2♥, 3♥, 4♥, 5♥, 6♥, 7♥, 8♥, 9♥, 10♥, J♥, Q♥, K♥, A♣, 2♣, 3♣, 4♣, 5♣, 6♣, 7♣, 8♣, 9♣, 10♣, J♣, Q♣, K♣, A♦, 2♦, 3♦, 4♦, 5♦, 6♦, 7♦, 8♦, 9♦, 10♦, J♦, Q♦, K♦"}
+
+#code for the actual game
+def game_main():
+    temp = "temporary code"
+
+#starting code
+def game_start():
+    gameStart = input("Start?")
+    
 
