5ef84a7516ba2f3d4b55240f4744a638006cff48
m1yaryu <127284546+m1yaryu@users.noreply.github.com>
2023-12-07 23:18:56 +0900

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -7,35 +7,29 @@ Cards = ["A♠", "2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠",
          "A♦", "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦"]
 
 #initializing dealer and player cards variables 
-dealer_cards = ""
-player_cards = ""
-player_cards1 = ""
+player_cards = []
+dealer_cards = []
+start_game = input("Start game?[Y/N] -> ")
 
 #game flow 
 def gameMain():
-    valueCalc()
-    playerMove()
+    while start_game == True:
+        player_cards.extend(random.sample(Cards, 2))
+        dealer_cards.extend(Cards, 2)
+        valueCalc()
+        playerMove()
 
 #performs program based on player's input/move
 def playerMove():
-    move = input("What's the move?(Hit/Stand/Double) --> ")
-    if move == stand:
-        stand()
+    move = input("What's the move?(Hit/Stand) --> ")
+    while move == True:
+        if move == stand:
+            move == False
     
-    elif move == hit:
-        player_cards += random.choice(Cards)
+        elif move == hit:
+            player_cards += random.choice(Cards)
     
-    elif move == double:
-        player_cards1 = player_cards + random.choice(Cards)
 
-def valueCalc():
+#def valueCalc():
 #    if dealer_cards == 
 #        print(f"dealer: {}")
-    
-
-
-
-    
-
-
-    
